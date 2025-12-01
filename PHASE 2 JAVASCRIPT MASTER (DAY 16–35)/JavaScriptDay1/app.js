be334ce213d573d06c865d9ada8792873b276c61
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-12-01 23:15:04 +0530

    JavaScipt

---
+++ PHASE 2 JAVASCRIPT MASTER (DAY 16–35)/JavaScriptDay1/app.js
@@ -0,0 +1,120 @@
+// document.write("Welcome")
+// document.write(19+12)
+// console.log('Welcome' ,32-+23)
+// var n =23
+// var f =34
+// console.log('Ans' , n+f)
+// var n =32
+// console.log('N Ki value' , n)
+
+
+// var username ='DS it s mens Devesh'
+// console.log(username)
+
+// let a =23;
+
+// let d =34;
+
+// {
+
+//     var s =12
+
+// }
+
+// console.log(s)
+
+
+// const n =10
+// console.log(n)
+// n=23
+// console.log(n)
+
+// {
+//     const d =23
+//     console.log(d)
+    
+// }
+
+
+// var d =34
+// console.log(d)
+
+
+// console.log(n)
+// var n =37
+
+
+// userName = "devesh";
+// console.log(userName)
+
+// Template literals
+
+// let n =10
+
+
+// console.log(`this is number ${n} Ok Bro `)
+
+// DataTypes in JS
+
+// Type               	Description
+// String	               A text of characters enclosed in quotes
+// Number	               A number representing a mathematical value
+// Bigint	               A number representing a large integer
+// Boolean	               A data type representing true or false
+// Object	               A collection of key-value pairs of data
+// Undefined              	A primitive variable with no assigned value
+// Null	                    A primitive value representing object absence
+// Symbol	                A unique and primitive identifier
+
+// let n =12;
+// console.log(typeof n )
+// let w ='Welcome'
+// console.log(typeof w )
+
+// let f
+// console.log(typeof f)
+// let status = true
+// console.log(typeof status)
+
+// console.log(true+1)
+//null
+// let d = null
+// console.log(typeof d)
+// Symbol
+// let p= Symbol('Hello')
+// console.log(p)
+// bigint
+
+// non-primitive data types
+// let  I=[23,4,5,6,7,8,9,]
+// object 
+// let obj={
+//     userName:"Devsh",
+//     age:23
+// }
+
+// function 
+
+// function showdata() {
+
+    
+// }
+// console.log(typeof showdata)
+
+
+
+
+// let n=2
+// console.log(n*"10") // 20
+
+// console.log(n+"6") //26
+// console.log("10" -20)
+
+
+
+// let num1 = Number=prompt("Enter the value")
+// console.log(num1)
+
+
+// * Let's Practise *
+// 1. create three variables: Name , age and is MediaStreamAudioDestinationNode, Assing them appropriate value and print them using console.log () .
---
+++ PHASE 2 JAVASCRIPT MASTER (DAY 16–35)/javaScriptDay2/app.js
@@ -0,0 +1,243 @@
+// mathematical operations 
+
+// let n =23;
+// let m = 34;
+
+// console.log(m+n)
+// console.log(m-n)
+// console.log('n-m')
+
+
+//models
+// console.log(m%4)
+
+
+// n++,n++
+// n--
+// n=n+1
+
+// console.log('n is' , n)
+
+// console.log(5**3)
+
+//
+
+// Assignment Operators
+
+// let n =10
+// console.log(n)
+// n=n+5
+
+// n+=5
+// console.log(n)
+
+
+// Qus
+// let a=10
+// let b=20
+
+// let temb1 = a
+
+// a=b
+// b=temb1
+
+// [a, b] = [b, a]
+
+
+// console.log(`a is ${a} and b is ${b}`)
+
+// comparison operators
+
+
+
+// let a= 10
+// let b =20
+// ==
+// console.log(a==b)
+// console.log(a=="10")
+
+// ===
+// console.log(a===b)
+//  console.log(20!=a)
+//  console.log(20!=20)
+//  console.log(1!==b)
+
+// console.log(n<m)
+
+// console.log(30>=60)
+
+
+
+
+// console.log( (30>50) +5
+//   )
+
+
+//   let a = 23
+//   let b =20
+//   let c=45
+
+//   console.log(a<b<c)
+
+
+  // Logical Operators ----------------^
+
+//   let username = "code"
+//   let password = 2345
+
+//   console.log(username== "code" && password=="2345")
+
+//   // 
+//   console.log(10==20 || 10> 20)
+
+//   console.log( !20 !=20)
+
+//   console.log( ! true)
+
+
+  // String operators 
+
+//   let str1 ="Ws"
+//   let str2 =" Welcome"
+//   console.log(str1+str2)
+
+// // Ternary Operator 
+
+// let age = 18
+// let result =(age >=18  ) ? "Adult" : "Minor"
+// console.log(result)
+
+
+// let age =Number(prompt ("Enter Your age"))
+
+
+// // let result=age>=18 ? "Yes"   :  "False"
+// console.log(result)
+
+
+// let num1 = Number(prompt ('Enter the first Number'))
+// let num2 = Number(prompt ('Enter the first Second'))
+
+// let result = num1>num2 ? `num1 is ${num1} is greater then num2 ${num2} ` : `num2 is ${num2} is greater then num1 ${num1}`
+
+
+
+
+
+
+
+// Conditional Statement z
+
+
+if(true){
+    console.log("hello")
+
+}
+ let score1 =80
+if(score1>60){
+    console.log("You passed")
+}
+
+console.log("Welcome")
+
+
+
+
+
+let score =90
+
+if (score>50) {
+    console.log("passed")
+    
+} else {
+    console.log("Fail")
+    
+}
+
+// Task 1
+
+// let num1 =Number(prompt('Enter the value'))
+// if(num1%2==0){
+//     console.log("Even Number")
+// }
+// else{
+//     console.log(num1 ,"odd Number")
+// }
+
+
+// if else if else ladder
+
+// let marks =Number(prompt("Enter Your Number"))
+// if (marks>=90) {
+//     console.log(marks, "wow you got A")
+    
+// } else if(marks>80) {
+    
+//     console.log(marks, "wow you got B ")
+
+    
+// }
+// else{
+//     console.log(" Oh No! try Again ")
+// }
+
+
+// Nested if Else 
+
+// let userName ="admin"
+// let password =2456
+// let =UserNameInput =prompt("enter the username")
+// if(UserNameInput == userName){
+//     // console.log("Nice UserName toh Sahi ha bhai")
+//     let passwordInput =prompt("enter the password")
+//     {
+//         console.log("Login Done")
+//     }
+
+// } else{
+//     console.log("UserNmae Sahi nhi baba")
+// }
+
+
+// Switch case
+
+// let Lights ="Yellow"
+
+// switch(Lights){
+//     case "red":
+//     console.log("stop");
+//     break
+
+//     case "Yellow":
+//         console.log("reday");
+//         break
+
+//     case "Green":
+//         console.log("Go");
+//         break
+
+//         default:
+//             console.log("Invalid Light")
+//             break
+
+
+// }
+
+let num1 =Number(prompt("Enter the Value One"))
+let num2 =Number(prompt ("Enter the Value Two"))
+let opr =prompt (`
+    1 Add (+)
+    2 Min (-)
+    3 mul (*)
+
+    
+    `)
+    switch(opr){
+        case 
+    }
+
+
+
+
+
+
