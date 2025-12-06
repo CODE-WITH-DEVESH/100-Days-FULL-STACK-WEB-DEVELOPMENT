e34044f109e51ef9f35df65cf72c7121bb91aa39
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-12-06 23:07:25 +0530

    Today i have done loop and many conten

---
+++ PHASE 2 JAVASCRIPT MASTER (DAY 16–35)/javaScriptDay2/tesk2.js
@@ -0,0 +1,78 @@
+
+// let user =["ram","ravi","Dev",12,23]
+// let checkLength = user.length
+
+
+// for(let i=checkLength-1;i>=0; i--){
+//     console.log(i.user[i])
+// }
+
+
+
+// let l =[12,24,56,78,86,88]
+// let sum =0
+// for(let v of l ){
+//     sum+=v
+//     console.log(v)
+// }
+
+// console.log("Array Sum" ,sum)
+
+// for(let k in l ){
+//     console.log(k)
+// }
+
+
+
+// let l =[23,34,5,6,55,6,7,84,45,67]
+// for(let v of l){
+//     if(v%2==0){
+//         console.log(v)
+//     }
+// }
+
+
+// let l=[1,2,3,4]
+// console.log(l)
+// l[2] =30
+// console.log(l)
+
+
+// Push ()
+
+// let l =[10,20,30,45]
+// // l.push(99)
+// // l.push(44)
+// l.push(99,66,55)
+// console.log(l)
+
+
+// let l =[23,4,5,6,8]
+// console.log(l.pop())
+// console.log(l)
+
+// console.log(l.shift())
+// console.log(l)
+
+// l.unshift(24)
+// console.log(l)
+// l.splice(1,2)
+
+// console.log(l)
+
+// l.splice(1,2,45,66,77,88)
+// console.log(l)
+
+
+// let l = [23,44,56,78,99,0,33,44]
+// l.sort()
+// console.log(l)
+// l.reverse()
+// console.log(l)
+
+
+// l.fill(5,0,2)
+// console.log(l)
+
+
+
---
+++ PHASE 2 JAVASCRIPT MASTER (DAY 16–35)/javaScriptDay2/tesk3.js
@@ -0,0 +1,9 @@
+// let l =[1,2,3,4]
+// // console.log(l)
+// for(let v of l){
+//     console.log(v)
+//     l.pop()
+// }
+
+// Qustion
+let l =[1,2,3,4,5,6]
