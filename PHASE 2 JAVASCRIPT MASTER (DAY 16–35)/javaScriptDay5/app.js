8ba2c7f98f8965c25d36312c91bbab827547c4db
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2026-01-01 11:40:37 +0530

    Dom Done

--- PHASE 2 JAVASCRIPT MASTER (DAY 16–35)/javaScriptDay5/app.js
+++ PHASE 2 JAVASCRIPT MASTER (DAY 16–35)/javaScriptDay5/app.js
@@ -1,80 +1,80 @@
-// let headingElement =document.getElementById("heading")
+// // let headingElement =document.getElementById("heading")
 
-// let para1 =document.getElementById("para1")
+// // let para1 =document.getElementById("para1")
 
-// console.log(headingElement.innerHTML)
-// console.log(para1.innerText)
-// Query Selector ()
+// // console.log(headingElement.innerHTML)
+// // console.log(para1.innerText)
+// // Query Selector ()
 
-// let inputgmil=document.querySelector('#gmail')
-// console.log(inputname.placeholder);
-// console.log(inputgmil.type);
-// inputname.placeholder="Enter your name"
-// inputgmil.placeholder ="Enter your Gmail"
-// inputname.type="password"
+// // let inputgmil=document.querySelector('#gmail')
+// // console.log(inputname.placeholder);
+// // console.log(inputgmil.type);
+// // inputname.placeholder="Enter your name"
+// // inputgmil.placeholder ="Enter your Gmail"
+// // inputname.type="password"
 
-// console.log(inputgmil.placeholder);
+// // console.log(inputgmil.placeholder);
 
-// console.log(inputname.value);
+// // console.log(inputname.value);
 
-// Qusery All Selecort()
-
-
-// let allelement = document.querySelectorAll('li')
-// console.log(allelement);
-// console.log( allelement[0].innerHTML);
-// console.log( allelement[1].innerHTML);
-// console.log( allelement[2].innerHTML);
-// console.log( allelement[3].innerHTML);
+// // Qusery All Selecort()
 
 
-// allelement.forEach((item,index)=>{
-//     console.log(item.innerHTML)
-// })
+// // let allelement = document.querySelectorAll('li')
+// // console.log(allelement);
+// // console.log( allelement[0].innerHTML);
+// // console.log( allelement[1].innerHTML);
+// // console.log( allelement[2].innerHTML);
+// // console.log( allelement[3].innerHTML);
 
-// allelement.forEach(m=>{
-//     console.log(m.innerHTML);
 
-// })
+// // allelement.forEach((item,index)=>{
+// //     console.log(item.innerHTML)
+// // })
+
+// // allelement.forEach(m=>{
+// //     console.log(m.innerHTML);
+
+// // })
+
+
+// // let inputpassword = document.querySelector('#password')
+
+// // let buttonbtn = document.querySelector('#btn')
+
+
+
+// // function showHidePassword() {
+
+// //     if (buttonbtn.innerHTML == "show") {
+
+// //         inputpassword.type = "text"
+// //         buttonbtn.type = "hide"
+// //     }
+
+// //     else {
+// //         inputpassword.type = "password"
+// //         buttonbtn.innerHTML = "show"
+// //     }
+
+
+// // }
 
 
 // let inputpassword = document.querySelector('#password')
-
 // let buttonbtn = document.querySelector('#btn')
-
-
 
 // function showHidePassword() {
 
-//     if (buttonbtn.innerHTML == "show") {
-
+//     if (buttonbtn.innerHTML === "Show") {
 //         inputpassword.type = "text"
-//         buttonbtn.type = "hide"
-//     }
-
+//         buttonbtn.innerHTML = "Hide"
+//     } 
 //     else {
 //         inputpassword.type = "password"
-//         buttonbtn.innerHTML = "show"
+//         buttonbtn.innerHTML = "Show"
 //     }
-
-
 // }
-
-
-let inputpassword = document.querySelector('#password')
-let buttonbtn = document.querySelector('#btn')
-
-function showHidePassword() {
-
-    if (buttonbtn.innerHTML === "Show") {
-        inputpassword.type = "text"
-        buttonbtn.innerHTML = "Hide"
-    } 
-    else {
-        inputpassword.type = "password"
-        buttonbtn.innerHTML = "Show"
-    }
-}
 
 
 
